5854dec49b3d7ed44f2b6f2e971bfa5ba4585ac4
Davide Pedrotti <unibox@duck.com>
2024-04-13 14:56:27 +0200

    fixed inline js

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 const input = document.getElementById('input');
 const liElements = document.querySelectorAll('li');
 
-function createElement(type) {
+function getEl(type) {
   const text = `H${type}: ${input.value}`;
   const output = document.getElementById('generated-elements');
   const element = document.createElement('h' + type);
   element.textContent = text;
-  output.appendChild(element);
+  output.insertAdjacentElement('beforeend', element);
 }
-liElements.forEach((el, idx) => {
-  el.addEventListener('click', ev => {
-    createElement(idx + 1);
-  });
-});
+// liElements.forEach((el, idx) => {
+//   el.addEventListener('click', ev => {
+//     createElement(idx + 1);
+//   });
+// });
